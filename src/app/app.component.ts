aae947b72c30db86061b17677339e0d4ec3e52ef
unknown <yury.siruts@giraffestudio.pl>
2022-06-08 23:37:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators/custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'email-validation-tutorial';
+  constructor(private fb: FormBuilder) {}
+
+  public userEmails: FormGroup = this.fb.group({
+    primaryEmail: [ '', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")] ],
+    secondaryEmail: [ '', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")] ],
+    password: [ null, Validators.compose([
+        // 1. Password Field is Required
+        Validators.required,
+        // 2. check whether the entered password has a number
+        CustomValidators.patternValidator(/\d/, { hasNumber: true }),
+        // 3. check whether the entered password has upper case letter
+        CustomValidators.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+        // 4. check whether the entered password has a lower-case letter
+        CustomValidators.patternValidator(/[a-z]/, { hasSmallCase: true }),
+        // 5. check whether the entered password has a special character
+        CustomValidators.patternValidator(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,100})/, { hasSpecialCharacters: true }),
+        // 6. Has a minimum length of 8 characters
+        Validators.minLength(8)
+      ]),
+    ],
+    confirmPassword: [null, Validators.compose([Validators.required])],
+  });
+
+  get primaryEmail() {
+    return this.userEmails.get('primaryEmail')
+  }
+  
+  get secondaryEmail() {
+    return this.userEmails.get('secondaryEmail')
+  }
+
+  get password() {
+    return this.userEmails.get('password')
+  }
+
+  submitForm() {
+
+  }
 }
